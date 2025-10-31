ca22661202d1af4fb717587660ffb54a200de2ec
Lorenzo Baravelli <LORENZO.BARAVELLI@local.ittcesena.it>
2025-10-31 09:42:05 +0100

    Get() e Set()

---
+++ CDbrani/Brano.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranoCD
+{
+    internal class Brano
+    {
+        public string _titolo { get; set; }
+        public string _autore { get; set; }
+        public double _durata
+        {
+            get { return _durata; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("non puoi inserire una durata negativa");
+                }
+                else
+                {
+                    _durata = value;
+                }
+                _durata = value;
+            }
+        }
+
+        public Brano(string titolo, string autore, double durata)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _durata = durata;
+
+        }
+        public string toString()
+        {
+            return _titolo;
+        }
+
+        public string GetTitolo()
+        {
+            return _titolo;
+        }
+
+        public double GetDurata()
+        {
+            return _durata;
+        }
+        public string GetAutore()
+        {
+            return _autore;
+        }
+
+        public string SetTitolo(string titolo)
+        {
+            _titolo = titolo;
+            return _titolo;
+        }
+
+        public string SetAutore(string autore)
+        {
+            _autore = autore;
+            return _autore;
+        }
+
+        public double SetDurata(double durata)
+        {
+            _durata = durata;
+            return _durata;
+        }
+
+
+    }
+}
--- CDbrani/obj/Debug/net8.0-windows/CDbrani.AssemblyInfo.cs
+++ CDbrani/obj/Debug/net8.0-windows/CDbrani.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDbrani")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ea4ea4d3cc3a57ec876a700142f083dc734d43")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDbrani")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDbrani")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
